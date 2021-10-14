323bdf339933ce32c6db861fd1f05bb8ca774028
kunchao322 <chao.k@northeastern.edu>
2021-10-13 20:51:15 -0700

    fixed Shop and cart button to right functions

--- shopping/shop/views.py
+++ shopping/shop/views.py
@@ -7,10 +7,22 @@ from .models import Category, Product
 # Create your views here.
 
 #request function
-def index(request):
-    text_var = 'This is the first DjangoApp webpage'
-    #hhttp response to render http
-    return HttpResponse(text_var)
+def index(request,c_slug = None):
+    c_page = None #Category
+    products = None
+    if c_slug != None:
+        c_page  = get_object_or_404(Category, slug = c_slug)
+        products = Product.objects.filter(Category=c_page,available = True)
+    else:
+        products = Product.objects.all().filter(available = True)
+    return render(request, 'shop/category.html', {'category':c_page, 'products':products}) #pass a dictionary
+ 
+def ProductCatDetail(request, c_slug, product_slug):
+    try:
+        product = Product.objects.get(category_slug = c_slug, slug = product_slug)
+    except Exception as e:
+        raise e
+    return render(request, 'shop/product.html',{'product':product})
 
 
 #ist category view
